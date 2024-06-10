1abc45dcb7750213ebaff8a87a878b1916a99b47
Aniket Dabhade <121627128+Aniket-Dabhade@users.noreply.github.com>
2024-06-10 09:26:16 +0530

    Add files via upload

---
+++ First project/project.py
@@ -0,0 +1,16 @@
+print("____________Welcome In My Quiz Game__________")
+ans=input("Do you want to start the game?").lower()
+if ans=="yes":
+    print("Lets begin")
+    q1=input("what is 3+9").lower()
+    if q1==  12 or q1=="twele":
+        print("yehhhh!!!,Right")
+    else :
+        print("bad luck")
+    q2 = input("what is colour of the sky").lower()
+    if q2 == "blue":
+        print("yehhhh!!!,Right")
+    else:
+        print("bad luck")
+else :
+    print("ok bye")
